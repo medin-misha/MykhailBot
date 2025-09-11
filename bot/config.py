e888afceef54ed77e69eaf68d62d4cfea7c87b27
misha <medinskijmisa228@gmail.com>
2025-09-11 14:50:58 +0300

    Added Zen link

--- bot/config.py
+++ bot/config.py
@@ -13,11 +13,12 @@ class BotSettings:
 
 class Messages:
     start: str = os.getenv("START_MESSAGE", "Привет, я бот который создаст твою <b>личную</b> таблицу жизни! Напиши мне /table что бы начать процесс создания.")
-    social: str = os.getenv("SOCIAL_MESSAGE", "Вот все мои медиа &lt;3")
+    social: str = os.getenv("SOCIAL_MESSAGE", "К стати, я тут подумал что хорошо, бы выложить ссылочки на мои социальные сети &lt;3")
     media_links: dict = {
         "YouTube": os.getenv("YOUTUBE_LINK", "https://www.youtube.com/@it_was_I_misha"),
         "Telegram": os.getenv("TELEGRAM_LINK", "https://t.me/medinmisha"),
         "Instagram": os.getenv("INSTAGRAM_LINK", "https://www.instagram.com/it_was_i_misha/"),
         "TikTok": os.getenv("TIKTOK_LINK", "https://www.tiktok.com/@iteasimisha"),
         "VK": os.getenv("VK_LINK", "https://vk.com/id766354665"),
+        "Дзен": os.getenv("ZEN_LINK", "https://dzen.ru/it_was_i_misha")
     }
